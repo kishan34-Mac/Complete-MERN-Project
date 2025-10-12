e43e2d072d4fcd796289169f9163c9506bce6e36
Kishan Singh <your-email@domain.com>
2025-10-12 13:08:28 +0530

    ”a”

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,15 +1,25 @@
-const userModel=require("../models/user.model")
+const userModel = require("../models/user.model");
+const bcryt = require("bcryptjs");
+const jwt =require('jsonwebtoken')
 
-async function registerUser(req ,res){
-    const {
-        fullName,email,password
-    }=req.body;
-    const isUserAlreadyExits=await userModel.findOne({
-        email
-    })
-    if (isUserAlreadyExits){
-        return res.status(400).json({
-            message:"The user is already Exist"
-        })
-    }
+async function registerUser(req, res) {
+  const { fullName, email, password } = req.body;
+  const isUserAlreadyExits = await userModel.findOne({
+    email,
+  });
+  if (isUserAlreadyExits) {
+    return res.status(400).json({
+      message: "The user is already Exist",
+    });
+  }
+  const hashedPassword = await bcryt.hash(password, 10);
+
+  const user = await userModel.create({
+    fullName,
+    email,
+    password: hashedPassword,
+  })
+  const token =jwt.sign({
+    id:user._id,
+  })
 }
