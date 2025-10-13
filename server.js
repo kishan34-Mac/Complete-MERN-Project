93165b39cba261f35132a7dc9e4ae9053855e4cf
Kishan Singh <your-email@domain.com>
2025-10-14 00:09:43 +0530

    ”a”

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const app = require("./src/app");
 const connectDB=require('./src/db/db')
 require('dotenv').config();
+console.log("Public Key:", process.env.IMAGEKIT_PUBLIC_KEY);
+
+
 
 connectDB()
 app.listen(3000,()=>{
--- src/controllers/food.controller.js
+++ src/controllers/food.controller.js
@@ -1,10 +1,14 @@
 const foodModel= require('../models/food.model');
+const strogeService=require("../services/stroage.service")
  
 // controllers/food.controller.js
 async function createFood(req, res) {
   // normal Express signature: (req, res)
   console.log(req.foodPartner);
   console.log(req.body)
+  console.log(req.file)
+  const fileUploadResult=await strogeService.uploadFile(req.file.buffer, req.file.originalname,"");
+
   
   res.json({ success: true, message: "Food created" });
 }
